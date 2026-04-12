21fbfe79df7138374420cee7f4f7f76d670c42f2
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-12 15:08:21 +0900

    New: FTP.class.php

---
+++ FTP.class.php
@@ -0,0 +1,38 @@
+<?php
+/**	op-unit-ftp:/FTP.class.php
+ *
+ * @created    2026-04-12
+ * @license    Apache-2.0
+ * @package    op-unit-ftp
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP\UNIT;
+
+/**	Use
+ *
+ */
+use OP\OP_CORE;
+use OP\OP_CI;
+use OP\IF_FTP;
+
+/**	FTP
+ *
+ * @created    2026-04-12
+ */
+class FTP implements IF_FTP
+{
+	/**	trait
+	 *
+	 */
+	use OP_CORE, OP_CI;
+}
