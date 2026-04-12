1f6a4f77405f36063b7de0daccaa76b073132e06
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-12 15:08:33 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-unit-ftp:/index.php
+ *
+ * @created    2026-04-12
+ * @license    Apache-2.0
+ * @package    op-unit-ftp
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	Include
+ *
+ */
+require_once(__DIR__.'/FTP.class.php');
