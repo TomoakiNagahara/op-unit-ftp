c411a9ee110b313851f13374984f4028223129d1
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-15 14:04:01 +0900

    New: Directory.class.php

---
+++ Directory.class.php
@@ -0,0 +1,38 @@
+<?php
+/**	op-unit-ftp:/Directory.class.php
+ *
+ * @created    2026-04-18
+ * @license    Apache-2.0
+ * @package    op-unit-ftp
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP\UNIT\FTP;
+
+/**	Use
+ *
+ */
+use OP\OP_CORE;
+use OP\OP_CI;
+use OP\IF_FTP_DIRECTORY;
+
+/**	Directory opration functions of FTP
+ *
+ * @created    2026-04-12
+ */
+class Directory implements IF_FTP_DIRECTORY
+{
+	/**	trait
+	 *
+	 */
+	use OP_CORE, OP_CI;
+}
