c8e0d55fed94c294f55d8cad083723de0c7242f6
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-18 15:37:55 +0900

    New: File -> Connection( FTP\Connection & $connection )

---
+++ ci/File/Connection.php
@@ -0,0 +1,33 @@
+<?php
+/**	op-unit-ftp:/ci/File/Connection.php
+ *
+ * @created    2026-04-19
+ * @version    1.0
+ * @package    op-unit-ftp
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+$method = basename(__FILE__);
+$method = explode('.', $method)[0];
+
+/* @var $ci \OP\UNIT\CI\CI_Config */
+
+/* @var $ftp \OP\UNIT\FTP */
+$ftp = OP()->Unit('FTP');
+$connection = $ftp->Connection();
+
+//	...
+$args   = $connection;
+$result = null;
+$ci->Set($method, $result, $args);
